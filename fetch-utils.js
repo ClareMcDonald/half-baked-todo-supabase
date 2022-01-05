d70ca451063448b12853f1212b51a5e9558db3a7
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-01-05 15:15:23 -0800

    work on displayTodos and getTodos

--- fetch-utils.js
+++ fetch-utils.js
@@ -5,7 +5,10 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 export async function createTodo(todo){
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
-
+    const response = await client
+        .from('todos')
+        .instert({ todo });
+    
     return checkError(response);
 }
 
@@ -17,7 +20,10 @@ export async function deleteAllTodos() {
 
 export async function getTodos() {
     // get all todos for this user from supabase
-
+    const response = await client
+        .from('todos')
+        .select();
+    
     return checkError(response);    
 }
 
--- todos/todos.js
+++ todos/todos.js
@@ -15,21 +15,36 @@ const todoForm = document.querySelector('.todo-form');
 const logoutButton = document.querySelector('#logout');
 const deleteButton = document.querySelector('.delete-button');
 
+
 todoForm.addEventListener('submit', async(e) => {
     // on submit, create a todo, reset the form, and display the todos
+    await createTodo();
+
 });
 
 async function displayTodos() {
     // fetch the todos
-    
-    // display the list of todos
+    const todos = await getTodos();
 
+    // display the list of todos
+    for (let todo of todos) {
+        const todoEl = document.createElement('p');
+        todoEl.textContent = todo;
+        todosEl.append(todoEl);
+
+        displayTodos();
+    }
+    console.log(todos)
     // be sure to give each todo an event listener
 
     // on click, complete that todo
 }
 
 // add an on load listener that fetches and displays todos on load
+window.addEventListener('load', async() => {
+    await getTodos();
+    await displayTodos();
+});
 
 logoutButton.addEventListener('click', () => {
     logout();
